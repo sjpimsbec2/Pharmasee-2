f225969498115c3fbade67bb103c7337bf5f02d9
raiiizz <raizen.sangalang@hoolisoftware.com>
2023-03-11 01:33:09 +0800

    fix med

--- pharma.js
+++ pharma.js
@@ -1,45 +1,72 @@
-document.getElementById('cm-p').addEventListener('click', function() {
-    // go to cm.html
-    window.location.href = 'cm.html';
+// document.getElementById('cm-p').addEventListener('click', function() {
+//     // go to cm.html
+//     window.location.href = 'cm.html';
 
-});
-
-
-// dc-p is the id of the button
-document.getElementById('dc-p').addEventListener('click', function() {
-    // go to dc.html
-    window.location.href = 'dc.html';
-
-});
-
-// lz-p is the id of the button
-document.getElementById('lz-p').addEventListener('click', function() {
-    // go to lz.html
-    window.location.href = 'lz.html';
-
-});
-
-// md-p is the id of the button
-document.getElementById('md-p').addEventListener('click', function() {
-    // go to md.html
-    window.location.href = 'md.html';
-
-});
-
-// tj-p is the id of the button
-document.getElementById('tj-p').addEventListener('click', function() {
-    // go to tj.html
-    window.location.href = 'tj.html';
-
-});
+// });
 
 
-// dl-p is the id of the button
-document.getElementById('dl-p').addEventListener('click', function() {
-    // go to dl.html
-    window.location.href = 'dl.html';
+// // dc-p is the id of the button
+// document.getElementById('dc-p').addEventListener('click', function() {
+//     // go to dc.html
+//     window.location.href = 'dc.html';
+
+// });
+
+// // lz-p is the id of the button
+// document.getElementById('lz-p').addEventListener('click', function() {
+//     // go to lz.html
+//     window.location.href = 'lz.html';
+
+// });
+
+// // md-p is the id of the button
+// document.getElementById('md-p').addEventListener('click', function() {
+//     // go to md.html
+//     window.location.href = 'md.html';
+
+// });
+
+// // tj-p is the id of the button
+// document.getElementById('tj-p').addEventListener('click', function() {
+//     // go to tj.html
+//     window.location.href = 'tj.html';
+
+// });
+
+
+// // dl-p is the id of the button
+// document.getElementById('dl-p').addEventListener('click', function() {
+//     // go to dl.html
+//     window.location.href = 'dl.html';
+
+// }
+// );
+
+// jnj 
+document.getElementById('jnj').addEventListener('click', function() {
+    // go to jnj.html
+    window.location.href = 'jnj.html';
 
 }
 );
+
+// ys 
+document.getElementById('ys').addEventListener('click', function() {
+    // go to ys.html
+    window.location.href = 'ys.html';
+
+}
+);
+
+// rs 
+document.getElementById('rs').addEventListener('click', function() {
+    // go to rs.html
+    window.location.href = 'rs.html';
+
+}
+);
+
+
+
 
 
