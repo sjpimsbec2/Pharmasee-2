4166d002f38752d2e06ed36f26868bc1135f29c3
raiiizz <raizen.sangalang@hoolisoftware.com>
2023-03-12 02:42:55 +0800

    fix

--- components.js
+++ components.js
@@ -1,3 +1,64 @@
+  ////// ===== modal ====== /////
+  var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+
+  span.onclick = function() {
+  modal.style.display = "none";
+}
+
+function openModalPls(obj, img){
+    console.log(obj);
+
+    // clear all the data
+     var data = document.getElementsByClassName("data-here")[0];
+     data.innerHTML = "";
+ 
+     /** 
+     Category: ___
+ Description: ___
+ Prescription: (kasi need daw po yung "Over-the-counter" na word)
+ Stock: __ **/
+    modal.style.display = "block";
+   // get modal class but not the first one
+     var modalClass = modal.classList[1] + " --- " + name;
+     
+   // add data
+     var data = document.getElementsByClassName("data-here")[0];
+     console.log(document.getElementsByClassName("data-here"));
+     console.log(data);
+     // get the second class
+     var secondClass = data.classList[0];
+     // add the store name
+
+     ////////////
+     var image= document.createElement("img");
+     // add class
+     image.classList.add("img-responsive");
+     image.src = img;
+     data.appendChild(image);
+    ////////////
+    
+    
+
+ 
+   
+ 
+     
+  
+}
+
 function ComponentCard(obj) {
     return `<div class="column ">
     <div class="column">
@@ -5,7 +66,7 @@ function ComponentCard(obj) {
         ${obj.store}
 </div>
     <div class="card ${obj.name}">
-      <img onclick="openModalPls('${removeSingleQuote(obj.store)}')" class="card-test" src=${obj.image} alt="${obj.name}" >
+      <img onclick="openModalPls('${removeSingleQuote(obj.store)}', '${obj.image}')" class="card-test" src=${obj.image} alt="${obj.name}" >
       <div class="price-tag" id="font-card">
         ${obj.price}
       </div>
